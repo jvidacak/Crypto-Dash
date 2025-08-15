7a8b52aa882cb9826219f2d611a392467edeae1b
jvidacak <jvidacak123@gmail.com>
2025-08-15 18:20:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+**Crypto Dash**
+
+React dashboard for tracking crypto markets with CoinGecko API.
+Sort, search, and filter coins in real time with a clean UI.
+
+🚀 Features
+
+Live prices & market caps (CoinGecko)
+
+Sort by price, market cap, or 24h % change (asc/desc)
+
+Search by name or symbol
+
+Limit results (10 / 25 / 50 / 100)
+
+24h time format for updates
+
+Fully responsive
+
+🛠 Tech
+
+**React** (useState, useEffect)
+
+**CoinGecko API** /coins/markets
+
+**Styling**:  CSS Modules 
---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import { useState, useEffect } from "react";
+import CoinCard from "./components/CoinCard";
+import LimitSelector from "./components/LimitSelector";
+import FilterInput from "./components/FilterInput";
+import SortSelector from "./components/SortSelector";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const App = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(10);
+  const [filter, setFilter] = useState("");
+  const [sortBy, setSortBy] = useState("market_cap_desc");
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(
+          `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false;`
+        );
+        if (!res.ok) throw new Error("Failed to fetch data");
+        const data = await res.json();
+        setCoins(data);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCoins();
+  }, [limit]);
+
+  const filteredCoins = coins
+    .filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(filter.toLowerCase())
+    )
+    .slice()
+    .sort((a, b) => {
+      switch (sortBy) {
+        case "market_cap_desc":
+          return b.market_cap - a.market_cap;
+        case "market_cap_asc":
+          return a.market_cap - b.market_cap;
+        case "price_desc":
+          return b.current_price - a.current_price;
+        case "price_asc":
+          return a.current_price - b.current_price;
+        case "change_desc":
+          return b.price_change_percentage_24h - a.price_change_percentage_24h;
+        case "change_asc":
+          return a.price_change_percentage_24h - b.price_change_percentage_24h;
+        default:
+          return 0;
+      }
+    });
+
+  return (
+    <div>
+      <h1>🚀Crypto Dash</h1>
+      {loading && <p>Loading...</p>}
+      {error && <div className="error">{error}</div>}
+      <div className="top-controls">
+        <FilterInput filter={filter} onFilterChange={setFilter} />
+        <LimitSelector limit={limit} onLimitChange={setLimit} />
+        <SortSelector sortBy={sortBy} onSortChange={setSortBy} />
+      </div>
+
+      {!loading && !error && (
+        <main className="grid">
+          {filteredCoins.length > 0 ? (
+            filteredCoins.map((coin) => <CoinCard key={coin.id} coin={coin} />)
+          ) : (
+            <p>No coins found</p>
+          )}
+        </main>
+      )}
+    </div>
+  );
+};
+
+export default App;
