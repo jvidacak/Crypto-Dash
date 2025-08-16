51f97e2b5b4110c36809247c3a8e074960148b57
jvidacak <jvidacak123@gmail.com>
2025-08-16 17:11:11 +0200

    added chart and spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+import { BarLoader } from "react-spinners";
+
+const cssOverride = {
+        display: "block",
+        margin: "0 auto 50px auto"
+    };
+
+const Spinner = ({color = 'blue', size = '50'}) => {
+    return ( <div>
+        <BarLoader color={color} size={size} cssOverride={cssOverride} aria-label='Loading...' />
+    </div> );
+}
+ 
+export default Spinner;
